32a984cb6d12f20e536d4d112fa5a46ba7b43563
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-23 11:07:35 -0300

    Ahora al iniciar la app se conecta a la DB

--- Back/app.js
+++ Back/app.js
@@ -4,21 +4,20 @@ const express = require("express");
 
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
+const sequelize = require("./util/dataBase");
 
 app.use(express.json());
 
 app.use("/api-warmup", apiRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log("We´re connected to the database");
-  console.log("Running on port: " + process.env.PORT);
-  //   sequelize
-  //     .sync()
-  //     .then(() => {
-  //       console.log("We´re connected to the database");
-  //       console.log("Running on port: " + process.env.PORT);
-  //     })
-  //     .catch((error) => {
-  //       console.log("An error ocurred", error);
-  //     });
+  sequelize
+    .sync()
+    .then(() => {
+      console.log("Running on port: " + process.env.PORT);
+      console.log("We´re connected to the database");
+    })
+    .catch((error) => {
+      console.log("An error ocurred", error);
+    });
 });
