8e8a02447db38136d6cfb7ca44ba4688c8b277fd
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-23 14:31:47 -0300

    Nuevo endPoint para borrar

--- Back/routes/apiRoutes.js
+++ Back/routes/apiRoutes.js
@@ -8,5 +8,6 @@ router.get("/", apiController.getAllPosts);
 router.post("/", apiController.createPosts);
 router.get("/:oid", apiController.getDetailedPost);
 router.patch("/:oid", apiController.modifyPost);
+router.delete("/:oid", apiController.deletePost);
 
 module.exports = router;
