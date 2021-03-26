f98b4349c8083c70d43fb45fd41b009bd036bcb4
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-26 16:40:33 -0300

    Cambio en identacion

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -1,24 +1,31 @@
 import { useState, useEffect } from "react";
 
-import { StyledWrapper } from "./styles";
+import { StyledWrapper, StyledButton } from "./styles";
 
 import { getAllPosts } from "../../helper/axios/axios";
 import { mappingData } from "./utils";
 
 const HomePage = (props) => {
-  const [postsList, setPostsList] = useState([]);
+    const [postsList, setPostsList] = useState([]);
 
-  useEffect(() => {
-    getAllPosts()
-      .then((response) => {
-        if (response.status === 200) {
-          setPostsList(response.data);
-        }
-      })
-      .catch((error) => console.log(error));
-  }, []);
+    useEffect(() => {
+        getAllPosts()
+            .then((response) => {
+                if (response.status === 200) {
+                    setPostsList(response.data);
+                }
+            })
+            .catch((error) => console.log(error));
+    }, []);
 
-  return <StyledWrapper>{mappingData(postsList, props.history)}</StyledWrapper>;
+    return (
+        <StyledWrapper>
+            <StyledButton onClick={() => props.history.push("/addnew")}>
+                NEW POST
+            </StyledButton>
+            {mappingData(postsList, props.history)}
+        </StyledWrapper>
+    );
 };
 
 export default HomePage;
