5f82683ade633423b039d493442db7b79e54ffd2
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-26 16:41:51 -0300

    Cambio en identacion

--- front/src/pages/Home/utils.js
+++ front/src/pages/Home/utils.js
@@ -1,15 +1,17 @@
 import PostTab from "../../components/PostTab";
 
 export const mappingData = (data, history) => {
-  return data.map((eachPost) => {
-    return (
-      <PostTab
-        title={eachPost.title}
-        deleteHandler={() => console.log(`DELETED!!`)}
-        moreDetailsHandler={() => history.push("/detailedtab/" + eachPost.id)}
-        modifyHandler={() => console.log(`MODIFY!!`)}
-        key={eachPost.id}
-      />
-    );
-  });
+    return data.map((eachPost) => {
+        return (
+            <PostTab
+                title={eachPost.title}
+                deleteHandler={() => console.log(`DELETED!!`)}
+                moreDetailsHandler={() =>
+                    history.push("/detailedtab/" + eachPost.id)
+                }
+                modifyHandler={() => history.push("/")}
+                key={eachPost.id}
+            />
+        );
+    });
 };
