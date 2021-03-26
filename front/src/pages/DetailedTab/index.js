4ad22f46cf15520ceb2bd484972221203425e23a
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-26 13:56:14 -0300

    Cambio en identacion

--- front/src/pages/DetailedTab/index.js
+++ front/src/pages/DetailedTab/index.js
@@ -1,31 +1,39 @@
 import { useState, useEffect } from "react";
+
+import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
+
+import IconWrapper from "../../components/IconWrapper";
 import PostTab from "../../components/PostTab";
+import { StyledWrapper } from "./styles";
 
 import { getSinglePostData } from "../../helper/axios/axios";
 
-import { StyledWrapper } from "./styles";
-
 const DetailedTabPage = (props) => {
-  const [singlePost, setSinglePost] = useState([]);
+    const [singlePost, setSinglePost] = useState([]);
 
-  useEffect(() => {
-    const id = props.match.params.id;
-    getSinglePostData(id)
-      .then((response) => {
-        if (response.status === 200) {
-          setSinglePost(response.data);
-        }
-      })
-      .catch((error) => console.log(error));
-  }, []);
-  return (
-    <StyledWrapper>
-      <PostTab
-        isDetailed
-        paragraf={singlePost.body}
-        title={singlePost.title}></PostTab>
-    </StyledWrapper>
-  );
+    useEffect(() => {
+        const id = props.match.params.id;
+        getSinglePostData(id)
+            .then((response) => {
+                if (response.status === 200) {
+                    setSinglePost(response.data);
+                }
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <StyledWrapper>
+            <IconWrapper
+                iconClickHandler={() => props.history.goBack()}
+                iconComponent={<KeyboardBackspaceIcon />}
+            />
+            <PostTab
+                isDetailed
+                paragraf={singlePost.body}
+                title={singlePost.title}></PostTab>
+        </StyledWrapper>
+    );
 };
 
 export default DetailedTabPage;
--- front/src/pages/DetailedTab/styles.js
+++ front/src/pages/DetailedTab/styles.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 import { fonts } from "../../common/styles/index";
 
 export const StyledWrapper = styled.main`
-  display: flex;
-  flex-flow: column;
-  padding: 10px 20px;
-  font-family: ${fonts.poppins};
-  letter-spacing: -0.35px;
+    display: flex;
+    flex-flow: column;
+    padding: 10px 20px;
+    font-family: ${fonts.poppins};
+    letter-spacing: -0.35px;
 `;
