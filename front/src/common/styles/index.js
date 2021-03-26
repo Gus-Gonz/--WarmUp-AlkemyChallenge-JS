33fcafe714803f3afc4c29f386f20b488cc7361b
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-26 12:20:26 -0300

    Se agregan la fuente poppins Se crea un archivo para los colores

---
+++ front/src/common/styles/index.js
@@ -0,0 +1,17 @@
+import "./importFont.css";
+
+//colors
+export const colors = {
+  gray: "#999999",
+  lightGray: "#EAEAEA",
+  darkGray: "#636262",
+  white: "#FFFFFF",
+  salmon: "#ff7074",
+  lightSalmon: "#fea085",
+};
+
+//fonts
+export const fonts = {
+  roboto: "'Roboto', sans-serif",
+  poppins: "'Poppins', 'sans-serif'",
+};
