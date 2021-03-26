ebf8dcd1a36fdad9977d20eb388025f2e73f7dab
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-26 16:41:10 -0300

    Se agrego un button en la Page Home para ir a agregar un nuevo post

--- front/src/pages/Home/styles.js
+++ front/src/pages/Home/styles.js
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 
-import { fonts } from "../../common/styles/index";
+import { fonts, colors } from "../../common/styles/index";
 
 export const StyledWrapper = styled.main`
-  display: flex;
-  flex-flow: column;
-  padding: 10px 20px;
-  font-family: ${fonts.poppins};
-  letter-spacing: -0.35px;
+    display: flex;
+    flex-flow: column;
+    padding: 10px 20px;
+    font-family: ${fonts.poppins};
+    letter-spacing: -0.35px;
+`;
+
+export const StyledButton = styled.button`
+    cursor: pointer;
+    height: 30px;
+    width: 100%;
+    border-radius: 10px;
+    border-bottom-left-radius: 0px;
+    border: 2px solid #ff7074;
+    font-weight: 600;
+    background-color: ${(props) => (props.delete ? colors.salmon : "white")};
+    color: ${(props) => (!props.delete ? colors.salmon : "white")};
 `;
