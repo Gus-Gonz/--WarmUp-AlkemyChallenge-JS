7b3274edc60c328a2ef904c94d52b582a19a2c19
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-26 14:04:39 -0300

    Cambios en identacion

--- front/src/components/PostTab/index.js
+++ front/src/components/PostTab/index.js
@@ -1,41 +1,41 @@
 import {
-  StyledTab,
-  StyledTitleWrapper,
-  StyledButtonWrapper,
-  StyledButton,
+    StyledTab,
+    StyledTitleWrapper,
+    StyledButtonWrapper,
+    StyledButton,
 } from "./styles";
 
 const PostTab = ({
-  isDetailed,
-  title,
-  paragraf,
-  deleteHandler,
-  moreDetailsHandler,
-  modifyHandler,
+    isDetailed,
+    title,
+    paragraf,
+    deleteHandler,
+    moreDetailsHandler,
+    modifyHandler,
 }) => {
-  const paragrafTab = isDetailed ? <p>{paragraf}</p> : null;
-  const buttons = !isDetailed ? (
-    <StyledButtonWrapper>
-      <StyledButton onClick={modifyHandler}>MODIFY</StyledButton>
-      <StyledButton delete={true} onClick={deleteHandler}>
-        DELETE
-      </StyledButton>
-    </StyledButtonWrapper>
-  ) : (
-    <StyledButton isDetailed onClick={modifyHandler}>
-      MODIFY
-    </StyledButton>
-  );
+    const paragrafTab = isDetailed ? <p>{paragraf}</p> : null;
+    const buttons = !isDetailed ? (
+        <StyledButtonWrapper>
+            <StyledButton onClick={modifyHandler}>MODIFY</StyledButton>
+            <StyledButton delete={true} onClick={deleteHandler}>
+                DELETE
+            </StyledButton>
+        </StyledButtonWrapper>
+    ) : (
+        <StyledButton isDetailed onClick={modifyHandler}>
+            MODIFY
+        </StyledButton>
+    );
 
-  return (
-    <StyledTab>
-      <StyledTitleWrapper onClick={moreDetailsHandler}>
-        <h3>{title}</h3>
-      </StyledTitleWrapper>
-      {paragrafTab}
-      {buttons}
-    </StyledTab>
-  );
+    return (
+        <StyledTab>
+            <StyledTitleWrapper onClick={moreDetailsHandler}>
+                <h3>{title}</h3>
+            </StyledTitleWrapper>
+            {paragrafTab}
+            {buttons}
+        </StyledTab>
+    );
 };
 
 export default PostTab;
--- front/src/components/PostTab/styles.js
+++ front/src/components/PostTab/styles.js
@@ -3,38 +3,39 @@ import styled from "styled-components";
 import { colors } from "../../common/styles/index";
 
 export const StyledTab = styled.div`
-  text-align: center;
-  border: solid 2px ${colors.salmon};
-  border-radius: 15px;
-  box-shadow: 0px 0px 20px ${colors.lightSalmon};
+    text-align: center;
+    border: solid 2px ${colors.salmon};
+    border-radius: 15px;
+    box-shadow: 0px 0px 20px ${colors.lightSalmon};
 
-  padding: 10px;
-  margin: 10px 5px;
+    padding: 10px;
+    margin: 10px 5px;
 
-  background-color: white;
+    background-color: white;
 
-  p {
-    text-align: justify;
-  }
+    p {
+        text-align: justify;
+    }
 `;
 
 export const StyledTitleWrapper = styled.div`
-  cursor: pointer;
+    cursor: pointer;
 `;
 
 export const StyledButtonWrapper = styled.div`
-  display: flex;
-  justify-content: space-around;
-  margin: 10px 5px;
+    display: flex;
+    justify-content: space-around;
+    margin: 10px 5px;
 `;
 
 export const StyledButton = styled.button`
-  cursor: pointer;
-  height: 30px;
-  width: ${(props) => (props.isDetailed ? "50%" : "35%")};
-  border-radius: 10px;
-  border-bottom-left-radius: 0px;
-  border: 2px solid #ff7074;
-  background-color: ${(props) => (props.delete ? colors.salmon : "white")};
-  color: ${(props) => (!props.delete ? colors.salmon : "white")};
+    cursor: pointer;
+    height: 30px;
+    width: ${(props) => (props.isDetailed ? "50%" : "35%")};
+    border-radius: 10px;
+    border-bottom-left-radius: 0px;
+    border: 2px solid #ff7074;
+    font-weight: 600;
+    background-color: ${(props) => (props.delete ? colors.salmon : "white")};
+    color: ${(props) => (!props.delete ? colors.salmon : "white")};
 `;
