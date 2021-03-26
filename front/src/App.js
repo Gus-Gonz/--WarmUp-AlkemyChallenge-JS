09011d685faa2bcc939012c54c055d30d092999b
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-26 14:04:21 -0300

    Se agrego una nueva ruta

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/Home";
 import DetailedTabPage from "./pages/DetailedTab";
+import AddNewPage from "./pages/AddNew";
 
 function App() {
-  return (
-    <Switch>
-      <Route path="/detailedtab/:id" component={DetailedTabPage} />
-      <Route path="/" exact component={HomePage} />
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route path="/detailedtab/:id" component={DetailedTabPage} />
+            <Route path="/addnew" component={AddNewPage} />
+            <Route path="/" exact component={HomePage} />
+        </Switch>
+    );
 }
 
 export default App;
